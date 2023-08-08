0fdebd419e3259f3e255e4cc28c33733443ecf63
Igor Klymenko <>
2023-08-08 18:00:22 +0300

    Added realization

--- src/main/java/oop_practice/cat_dog/animal/Animal.java
+++ src/main/java/oop_practice/cat_dog/animal/Animal.java
@@ -1,5 +1,18 @@
 package oop_practice.cat_dog.animal;
 
-public class Animal {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class Animal {
+    public String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void sound();
 }
--- src/main/java/oop_practice/cat_dog/cat/Cat.java
+++ src/main/java/oop_practice/cat_dog/cat/Cat.java
@@ -1,5 +1,14 @@
 package oop_practice.cat_dog.cat;
 
-public class Cat {
+import oop_practice.cat_dog.animal.Animal;
 
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("I'm a Cat. My name " + name + ". Mew");
+    }
 }
